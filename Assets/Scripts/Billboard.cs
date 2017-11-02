9605afe4313f531e86bbfbbc97708dfdf05f3d1f
ksmizer <ksmizer@csub.edu>
2017-11-02 16:46:30 -0700

    Add files via upload

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+	void Update()
+	{
+		transform.LookAt(Camera.main.transform.position, Vector3.up);
+	}
+}
