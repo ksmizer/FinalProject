d8b508e64dd9d4fd64f722252492ef8a955f409f
Rodrigo Garcia-Novoa <rodrigogg400@yahoo.com>
2018-03-13 21:57:37 -0700

    March13
    Trigger Stuff

---
+++ Assets/Scripts/DadTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DadTrigger : MonoBehaviour {
+
+	public GameObject DadStats;
+
+	void OnTriggerEnter(Collider other)
+	{
+		DadStats.SetActive(true);
+		Debug.Log ("object enter");
+	}
+	void OnTriggerStay(Collider other)
+	{
+		DadStats.SetActive(true);
+		Debug.Log ("object stay");
+	}
+	void OnTriggerExit(Collider other)
+	{
+		DadStats.SetActive(false);
+		Debug.Log ("object exit");
+	}
+
+	/*void testing(Collider other) {
+		while(true) {
+			OnTriggerEnter(other);
+			OnTriggerStay(other);
+			OnTriggerExit(other);
+		}
+	}*/
+}
---
+++ Assets/Scripts/MomTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomTrigger : MonoBehaviour {
+
+	public GameObject MomStats;
+
+	void OnTriggerEnter(Collider other)
+	{
+		MomStats.SetActive(true);
+		Debug.Log ("object enter");
+	}
+	void OnTriggerStay(Collider other)
+	{
+		MomStats.SetActive(true);
+		Debug.Log ("object stay");
+	}
+	void OnTriggerExit(Collider other)
+	{
+		MomStats.SetActive(false);
+		Debug.Log ("object exit");
+	}
+
+	/*void testing(Collider other) {
+		while(true) {
+			OnTriggerEnter(other);
+			OnTriggerStay(other);
+			OnTriggerExit(other);
+		}
+	}*/
+}
