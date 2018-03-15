793a76088c6ceed1a5bda781287eca0f91a77932
Rodrigo Garcia-Novoa <rodrigogg400@yahoo.com>
2018-03-15 00:42:21 -0700

    Travel and Hospital

--- Assets/Scripts/DadTrigger.cs
+++ Assets/Scripts/DadTrigger.cs
@@ -1,21 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DadTrigger : MonoBehaviour {
 
 	public GameObject DadStats;
+	public GameObject TravelScreen;
+	public Button backButton;
+	public bool backBool;
+	public bool canvasBool = false;
 
 	void OnTriggerEnter(Collider other)
 	{
-		DadStats.SetActive(true);
+		DadStats.gameObject.SetActive(true);
 		Debug.Log ("object enter");
 	}
+
 	void OnTriggerStay(Collider other)
 	{
-		DadStats.SetActive(true);
+		if (Input.GetKeyUp (KeyCode.I)) {
+			DadStats.gameObject.SetActive (false);
+			canvasBool = true;
+			TravelScreen.SetActive (true);
+			Button btn = backButton.GetComponent<Button> ();
+			btn.onClick.AddListener (TaskOnClick);
+		} else if (!canvasBool) {
+			DadStats.gameObject.SetActive (true);
+		}
 		Debug.Log ("object stay");
 	}
+
+	void TaskOnClick()
+	{
+		DadStats.gameObject.SetActive (true);
+		TravelScreen.SetActive(false);
+		backBool = true;
+		canvasBool = false;
+	}
+
 	void OnTriggerExit(Collider other)
 	{
 		DadStats.SetActive(false);
--- Assets/Scripts/MomTrigger.cs
+++ Assets/Scripts/MomTrigger.cs
@@ -1,21 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MomTrigger : MonoBehaviour {
 
 	public GameObject MomStats;
+	public GameObject HospitalScreen;
+	public Button backButton;
+	public bool backBool;
+	public bool canvasBool = false;
 
 	void OnTriggerEnter(Collider other)
 	{
-		MomStats.SetActive(true);
+		MomStats.gameObject.SetActive(true);
 		Debug.Log ("object enter");
 	}
+
 	void OnTriggerStay(Collider other)
 	{
-		MomStats.SetActive(true);
+		//if (!canvasBool)
+			//MomStats.gameObject.SetActive (true);
+	
+		if (Input.GetKeyUp (KeyCode.I)) {
+			MomStats.gameObject.SetActive (false);
+			canvasBool = true;
+			HospitalScreen.SetActive (true);
+			Button btn = backButton.GetComponent<Button> ();
+			btn.onClick.AddListener (TaskOnClick);
+		} else if (!canvasBool) {
+			MomStats.gameObject.SetActive (true);
+		}
+
+
+		//MomStats.gameObject.SetActive (true);
+		//Time.timeScale = 1;
+		//canvasBool = false;
+		//backBool = false;
 		Debug.Log ("object stay");
 	}
+
+	void TaskOnClick()
+	{
+		MomStats.gameObject.SetActive (true);
+		HospitalScreen.SetActive(false);
+		backBool = true;
+		canvasBool = false;
+	}
+
 	void OnTriggerExit(Collider other)
 	{
 		MomStats.SetActive(false);
